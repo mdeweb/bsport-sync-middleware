6e11118a6c97202e9148243735dbb97aad953a2f
Mattthieu Devaux <36301708+mdeweb@users.noreply.github.com>
2026-06-02 15:00:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ async function createMemberInSanctuaryPass(member, attempt = 1) {
 
   const payload = {
     email: member.email,
-    first_name: member.firstname,
-    last_name: member.lastname,
+    firstname: member.firstname,
+    lastname: member.lastname,
     membership: {
       joined_date: formatDate(member.date_joined),
       membership_id: member.membership_ID || undefined,
